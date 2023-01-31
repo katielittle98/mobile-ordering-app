6df448f4350a2713791c37ed2732d2c0ae3e8c4f
Katie Little <katieanne.460@gmail.com>
2023-01-31 11:05:36 -0800

    updated files

---
+++ data.js
@@ -0,0 +1,97 @@
+
+export const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "garlic", "mozarella"],
+        price: 10,
+        emoji: "images/pizzaemoji.png",
+        id: 0,
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "images/burger.png",
+        id: 1
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain", "hops", "yeast", "water"],
+        price: 8,
+        emoji: "images/beer.png",
+        id: 2
+    },
+    {
+        name: "Soda",
+        ingredients: ["carbonated water", "high fructose corn syrup", "caffeine"],
+        price: 6,
+        emoji: "images/soda.png",
+        id: 3
+    },
+    {
+        name: "French Fries",
+        ingredients: ["potatoes", "salt"],
+        price: 5,
+        emoji: "images/fries.png",
+        id: 4
+    },
+    {
+        name: "Hot Dog",
+        ingredients: ["pork", "ketchup", "mustard"],
+        price: 10,
+        emoji: "images/hotdog.png",
+        id: 5
+    },
+    {
+        name: "Burrito",
+        ingredients: ["beef", "pinto beans", "guacamole", "cheese"],
+        price: 11,
+        emoji: "images/burrito.png",
+        id: 6
+    },
+    {
+        name: "Taco",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 6,
+        emoji: "images/taco.png",
+        id: 7
+    },
+    {
+        name: "Sushi",
+        ingredients: ["crab", "cucumber", "avocado"],
+        price: 12,
+        emoji: "images/sushi.png",
+        id: 8
+    },
+    {
+        name: "Boba",
+        ingredients: ["tapioca", "milk", "tea"],
+        price: 7,
+        emoji: "images/boba.png",
+        id: 9
+    },
+    {
+        name: "Soft Pretzel",
+        ingredients: ["flour", "salt", "butter"],
+        price: 8,
+        emoji: "images/pretzel.png",
+        id: 10
+    },
+    {
+        name: "Sandwich",
+        ingredients: ["turkey", "cheese", "lettuce", "tomato"],
+        price: 9,
+        emoji: "images/sandwich.png",
+        id: 11
+    },
+    {
+        name: "Salad",
+        ingredients: ["lettuce", "tomato", "spinach", "ranch"],
+        price: 10,
+        emoji: "images/salad.png",
+        id: 12
+    }
+
+
+]
+
